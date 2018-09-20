fdc5a6a9039854f6eb5ece501d043693cf1ac92c
lakhdeep123 <34301460+lakhdeep123@users.noreply.github.com>
2018-09-20 16:16:06 +0530

    Update TestComponent.js

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,36 +1,95 @@
 import React, { Component } from 'react';
-import { testAction } from '../actions/testAction';
+import { adultIncrement ,adultDecrement,checkError } from '../actions/counterAction';
 import { connect } from 'react-redux';
-
+var that;
 class TestComponent extends Component {
     constructor(props) {
         super(props);
+        this.state={disabled:false};
+        that=this;
+
     }
-    
-    handleClick(e) {
+   
+    handlePlus(e)
+    {
         debugger;
-        this.props.testAction();
-    }  
+        that.props.adultIncrement(e.target.id);
+     
+        
+       
+    }
+   
+    handleMinus2(e)
+    {
+       var text= $("#text2").val();
+          text=parseInt(text)-1;
+          $("#text2").val(text);
+      
+
+    }
+    handleMinus3(e)
+    {
+       var text= $("#text3").val();
+          text=parseInt(text)-1;
+          $("#text3").val(text);
+      
+
+    }
 
     render () {
         return (
-            <div className="testButton" onClick={this.handleClick.bind(this)}>
-                {this.props.test.toString()}
-            </div>
-        )
+           
+<div>
+    <span>Adult : </span>
+<input type="button" id="adult" className="plus" disabled={this.state.disabled} onClick={this.handlePlus} value="+"></input>
+<input type="text" value={this.props.adult}  id="text1"></input>
+<input type="button" id="btnminus1" className="minus" onClick={this.handleMinus1} value="-"></input>
+
+<br/><br/> <span>Children : </span>
+<input type="button"  id="children" className="plus" disabled={this.state.disabled} onClick={this.handlePlus}value="+"></input>
+<input type="text"   value={this.props.children} id="text2"></input>
+<input type="button" id="btnminus1" className="minus" onClick={this.handleMinus2} value="-"></input>
+
+<br/><br/> <span>Infant : </span>
+<input type="button"  id="infant" className="plus" disabled={this.state.disabled} onClick={this.handlePlus} value="+"></input>
+<input type="text"   value={this.props.infant} id="text3"></input>
+<input type="button" id="btnminus1" className="minus" onClick={this.handleMinus3} value="-"></input>
+
+    </div>
+          
+)
     }
 }
 
 const mapStateToProps = (state) => {
-    debugger;
-    const { value } = state.test;
+   debugger;
+
+  
+   if(that!=undefined)
+   {
+       if(state.counter.error)
+     that.setState({disabled:true});
+     else
+     {
+       that.setState({disabled:false});
+     }
+   }
+  
+
+
+    const { adult, children, infant, error } = state.counter;
     return {
-        test: value
+        adult,
+        children,
+        infant,
+        error
     };
 };
 
 const mapActionsToProps = {
-    testAction
+    adultIncrement,
+    adultDecrement,
+    checkError
 }
   
 export default connect(mapStateToProps, mapActionsToProps)(TestComponent);
